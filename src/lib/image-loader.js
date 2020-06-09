a2ec090689eb4af81422bb5a6ec7d9abb185c888
Roee Shapira <roy.shap1@gmail.com>
2020-06-09 19:19:45 +0300

    Improved image loader.

--- src/lib/image-loader.js
+++ src/lib/image-loader.js
@@ -2,12 +2,21 @@ import { Loader } from 'pixi.js';
 
 import bunnyPng from '@assets/img/bunny.png';
 
+let isFirstRun = true;
 export async function imageLoader() {
 	return new Promise((resolve, reject) => {
 		const loader = Loader.shared;
-		loader.onError.add(reject);
 
-		loader.add('bunny', bunnyPng);
-		loader.load((_, resources) => resolve(resources));
+		if (isFirstRun) {
+			loader.onError.add(reject);
+
+			loader.add('bunny', bunnyPng);
+			loader.load((_, resources) => {
+				isFirstRun = false;
+				return resolve(resources);
+			});
+		} else {
+			return resolve(loader.resources);
+		}
 	});
 }
